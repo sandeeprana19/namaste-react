d3e10b0358cafee09d1339d502ce836da84627c5
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-03-02 02:25:39 +0530

    episode-05

--- src/App.js
+++ src/App.js
@@ -3,24 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import Body from "./components/Body";
 
-/**
- * Header
- * - Logo
- * - NavItems
- * Body
- * - Search
- * - RestaurantContainer
- *    - RestaurantCard
- *      - Img
- *      - Name of restro, Star rating, cuisine, delivery time, etc.
- * Footer
- * - Copyright
- * - Links
- * - Address
- * - Contact
- */
-
 const AppLayout = () => {
+  console.log(<Body />);
   return (
     <div className="app">
       <Header />
--- src/components/Body.js
+++ src/components/Body.js
@@ -3,48 +3,8 @@ import resList from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  // Local state variable - super powerful variable
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      info: {
-        id: "21921",
-        name: "KFC",
-        cloudinaryImageId: "e70lglh0w4p4l0erere3",
-        costForTwo: "₹350 for two",
-        cuisines: ["Indian", "Chinese", "Tandoor"],
-        avgRating: 3.4,
-        sla: {
-          deliveryTime: 36,
-        },
-      },
-    },
-    {
-      info: {
-        id: "21922",
-        name: "Domino",
-        cloudinaryImageId: "e70lglh0w4p4l0erere3",
-        costForTwo: "₹350 for two",
-        cuisines: ["Indian", "Chinese", "Tandoor"],
-        avgRating: 4.4,
-        sla: {
-          deliveryTime: 36,
-        },
-      },
-    },
-    {
-      info: {
-        id: "21923",
-        name: "MCD",
-        cloudinaryImageId: "e70lglh0w4p4l0erere3",
-        costForTwo: "₹350 for two",
-        cuisines: ["Indian", "Chinese", "Tandoor"],
-        avgRating: 4.1,
-        sla: {
-          deliveryTime: 36,
-        },
-      },
-    },
-  ]);
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
   return (
     <div className="body">
