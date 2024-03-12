1f7b3fc6845648125e31a4774937755ee6241c58
sandeep-rana-19 <96654527+sandeeprana191@users.noreply.github.com>
2024-03-13 00:54:37 +0530

    episode-06 learning in progess

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Header } from "./components/Header";
 import Body from "./components/Body";
 
 const AppLayout = () => {
-  console.log(<Body />);
   return (
     <div className="app">
       <Header />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5563167&lng=88.38876300000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // Optional chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
